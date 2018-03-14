3381b3f3a3e12838daceb37336fc9370c27e856c
Phhofm <philiphofmann@hotmail.com>
2018-03-14 16:19:51 +0100

    Sheep added algorithm pseudocode from book
    Added alphabetasearch pseudocode from book. I need to adapt still.

--- kingsheep/team/phhofm/Sheep.java
+++ kingsheep/team/phhofm/Sheep.java
@@ -4,14 +4,53 @@ import kingsheep.*;
 
 public class Sheep extends UzhShortNameCreature {
 
+    //copied from greedy
     private boolean noMoreFoodAvailable = false;
     private Move lastMove;
     private int counter = 0;
 
+    //own
+    private Move move;
+
+    //original
     public Sheep(Type type, Simulator parent, int playerID, int x, int y) {
         super(type, parent, playerID, x, y);
     }
 
+
+    //this is the algorithm for alpha-beta search taken from the book
+    //TODO adapt this pseudocode to real code and this scenario
+    //how do states look like exactly? make own types? make min and max and utility function
+    protected Move alphaBetaSearch(state){  //returns a move
+        int v = maxValue(state,a,b);
+        return m;                           //TODO return move that leads to state with value v
+    }
+
+    protected int maxValue(state,a,b){      //returns a utility value
+        if (terminalTest){
+            return utility(state);
+        }
+        int v = 0;
+        foreach(action in ACTIONS(state)) do
+            int v = MAX(int v, minValue(RESULT(state,action),a,b));
+            if v>=b then return v;
+            a = MAX(a,v);
+        return v;
+    }
+
+    protected int minValue(state,a,b){      //returns a utility value
+        if (terminalTest){
+            return utility(state);
+        }
+        int v = 2;
+        foreach(action in ACTIONS(state)) do
+            int v = MIN(int v, maxValue(RESULT(state,action),a,b));
+        if v>=a then return v;
+        b = MIN(b,v);
+        return v;
+    }
+
+    //copied from greedy
     private boolean isSquareSafe(Type map[][], Move move){
         int x, y;
 
@@ -68,6 +107,7 @@ public class Sheep extends UzhShortNameCreature {
         }
     }
 
+    //original
     protected void think(Type map[][]) {
 		/*
 		TODO
@@ -87,6 +127,7 @@ public class Sheep extends UzhShortNameCreature {
 		*/
         //move = Move.WAIT; //provided by exercise
 
+        //copied from greedy
         if(alive && !noMoreFoodAvailable){
             char[] objectives = new char[2];
             objectives[0] = 'r';
