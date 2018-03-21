9a4c374e8ecd151b611e25ba0a76a4af532489cd
Phhofm <philiphofmann@hotmail.com>
2018-03-21 18:28:11 +0100

    initializeMethod is finished i think

--- kingsheep/team/phhofm/Sheep.java
+++ kingsheep/team/phhofm/Sheep.java
@@ -76,112 +76,159 @@ public class Sheep extends UzhShortNameCreature {
         Square sheep = new Square(type, x, y, 0, 0, 0);
         //expand: Create neighbor squares. Add values and distances (+1 or root square distance). If they are not outside the board, and dont exist yet in the hashmap, add them to the hashmap and to the toExpand Queue (except distance is >10.
     }
+
     private void initializeMapWithValues(Square origin, int yPos, int xPos, Type map[][]) {
         //Add all valid neighbour Squares
         try {
 
             //check if outside map. Map is always 15x19 squares, see assignment. Or distance is too great because resource limits (time especially).
-            if(origin.yCoor < 0 || origin.distance > MAXDISTANCE){} else {
-                //create square up and assign distance
-                Square upsquare = new Square(map[yPos + 1][xPos], origin.xCoor, origin.yCoor + 1, origin.distance + 1, 0, 0);
-                //assign value to upsquare
-                if (upsquare.type.equals(Type.EMPTY)) {
-                    upsquare.value = 0;
-                } else if (upsquare.type.equals(Type.GRASS)) {
-                    upsquare.value = 1;
-                } else if (upsquare.type.equals(Type.RHUBARB)) {
-                    upsquare.value = 5;
-                } else if (upsquare.type.equals(Type.FENCE)) {
-                    upsquare.value = -999;
-                } else if (upsquare.type.equals(Type.WOLF1)) {
-                    upsquare.value = -999;
-                } else if (upsquare.type.equals(Type.WOLF2)) {
-                    upsquare.value = -999;
-                } else if (upsquare.type.equals(enemySheep)) {
-                    upsquare.value = -999;
-                } else if (upsquare.type.equals(mySheep)) {
-                    upsquare.value = 0;
+            if (origin.yCoor-1 < 0 || origin.distance > MAXDISTANCE) {
+            } else {
+                //checks if this exists already in the Hashmap
+                if (mapWithValues.containsKey(getStringCoordinate(origin, -1, 0))) {
                 } else {
-                    throw new java.lang.RuntimeException("could not determine type of upsquare to assign a value");
+                    //create square up and assign distance
+                    Square upsquare = new Square(map[yPos - 1][xPos], origin.xCoor, origin.yCoor - 1, origin.distance + 1, 0, 0);
+                    //assign value to upsquare
+                    if (upsquare.type.equals(Type.EMPTY)) {
+                        upsquare.value = 0;
+                    } else if (upsquare.type.equals(Type.GRASS)) {
+                        upsquare.value = 1;
+                    } else if (upsquare.type.equals(Type.RHUBARB)) {
+                        upsquare.value = 5;
+                    } else if (upsquare.type.equals(Type.FENCE)) {
+                        upsquare.value = -999;
+                    } else if (upsquare.type.equals(Type.WOLF1)) {
+                        upsquare.value = -999;
+                    } else if (upsquare.type.equals(Type.WOLF2)) {
+                        upsquare.value = -999;
+                    } else if (upsquare.type.equals(enemySheep)) {
+                        upsquare.value = -999;
+                    } else if (upsquare.type.equals(mySheep)) {
+                        upsquare.value = 0;
+                    } else {
+                        throw new java.lang.RuntimeException("could not determine type of upsquare to assign a value");
+                    }
+                    //add to hashmap
+                    mapWithValues.put(getStringCoordinate(upsquare, 0, 0), upsquare);
+                    //add to expandQueue
+                    squareInitializeExpandQueue.add(upsquare);
                 }
             }
 
             //check if outside map. Map is always 15x19 squares, see assignment. Or distance is too great because resource limits (time especially).
-            if(origin.xCoor > 18|| origin.distance > MAXDISTANCE){} else {
-                Square rightsquare = new Square(map[yPos][xPos + 1], origin.xCoor + 1, origin.yCoor, origin.distance + 1, 0, 0);
-                if (rightsquare.type.equals(Type.EMPTY)) {
-                    rightsquare.value = 0;
-                } else if (rightsquare.type.equals(Type.GRASS)) {
-                    rightsquare.value = 1;
-                } else if (rightsquare.type.equals(Type.RHUBARB)) {
-                    rightsquare.value = 5;
-                } else if (rightsquare.type.equals(Type.FENCE)) {
-                    rightsquare.value = -999;
-                } else if (rightsquare.type.equals(Type.WOLF1)) {
-                    rightsquare.value = -999;
-                } else if (rightsquare.type.equals(Type.WOLF2)) {
-                    rightsquare.value = -999;
-                } else if (rightsquare.type.equals(enemySheep)) {
-                    rightsquare.value = -999;
-                } else if (rightsquare.type.equals(mySheep)) {
-                    rightsquare.value = 0;
+            if (origin.xCoor+1 > 18 || origin.distance > MAXDISTANCE) {
+            } else {
+                //checks if exists already in Hashmap
+                if (mapWithValues.containsKey(getStringCoordinate(origin, 0, 1))) {
                 } else {
-                    throw new java.lang.RuntimeException("could not determine type of rightsquare to assign a value");
+                    //TODO check if this exists already in the Hashmap
+                    Square rightsquare = new Square(map[yPos][xPos + 1], origin.xCoor + 1, origin.yCoor, origin.distance + 1, 0, 0);
+                    if (rightsquare.type.equals(Type.EMPTY)) {
+                        rightsquare.value = 0;
+                    } else if (rightsquare.type.equals(Type.GRASS)) {
+                        rightsquare.value = 1;
+                    } else if (rightsquare.type.equals(Type.RHUBARB)) {
+                        rightsquare.value = 5;
+                    } else if (rightsquare.type.equals(Type.FENCE)) {
+                        rightsquare.value = -999;
+                    } else if (rightsquare.type.equals(Type.WOLF1)) {
+                        rightsquare.value = -999;
+                    } else if (rightsquare.type.equals(Type.WOLF2)) {
+                        rightsquare.value = -999;
+                    } else if (rightsquare.type.equals(enemySheep)) {
+                        rightsquare.value = -999;
+                    } else if (rightsquare.type.equals(mySheep)) {
+                        rightsquare.value = 0;
+                    } else {
+                        throw new java.lang.RuntimeException("could not determine type of rightsquare to assign a value");
+                    }
+                    //add to hashmap
+                    mapWithValues.put(getStringCoordinate(rightsquare, 0, 0), rightsquare);
+                    //add to expandQueue
+                    squareInitializeExpandQueue.add(rightsquare);
                 }
             }
 
             //check if outside map. Map is always 15x19 squares, see assignment. Or distance is too great because resource limits (time especially).
-            if(origin.yCoor < 0|| origin.distance > MAXDISTANCE){} else {
-                Square leftsquare = new Square(map[yPos][xPos - 1], origin.xCoor - 1, origin.yCoor, origin.distance + 1, 0, 0);
-                if (leftsquare.type.equals(Type.EMPTY)) {
-                    leftsquare.value = 0;
-                } else if (leftsquare.type.equals(Type.GRASS)) {
-                    leftsquare.value = 1;
-                } else if (leftsquare.type.equals(Type.RHUBARB)) {
-                    leftsquare.value = 5;
-                } else if (leftsquare.type.equals(Type.FENCE)) {
-                    leftsquare.value = -999;
-                } else if (leftsquare.type.equals(Type.WOLF1)) {
-                    leftsquare.value = -999;
-                } else if (leftsquare.type.equals(Type.WOLF2)) {
-                    leftsquare.value = -999;
-                } else if (leftsquare.type.equals(enemySheep)) {
-                    leftsquare.value = -999;
-                } else if (leftsquare.type.equals(mySheep)) {
-                    leftsquare.value = 0;
+            if (origin.xCoor-1 < 0 || origin.distance > MAXDISTANCE) {
+            } else {
+                //checks if exists already in Hashmap
+                if (mapWithValues.containsKey(getStringCoordinate(origin, 0, -1))) {
                 } else {
-                    throw new java.lang.RuntimeException("could not determine type of leftsquare to assign a value");
+                    Square leftsquare = new Square(map[yPos][xPos - 1], origin.xCoor - 1, origin.yCoor, origin.distance + 1, 0, 0);
+                    if (leftsquare.type.equals(Type.EMPTY)) {
+                        leftsquare.value = 0;
+                    } else if (leftsquare.type.equals(Type.GRASS)) {
+                        leftsquare.value = 1;
+                    } else if (leftsquare.type.equals(Type.RHUBARB)) {
+                        leftsquare.value = 5;
+                    } else if (leftsquare.type.equals(Type.FENCE)) {
+                        leftsquare.value = -999;
+                    } else if (leftsquare.type.equals(Type.WOLF1)) {
+                        leftsquare.value = -999;
+                    } else if (leftsquare.type.equals(Type.WOLF2)) {
+                        leftsquare.value = -999;
+                    } else if (leftsquare.type.equals(enemySheep)) {
+                        leftsquare.value = -999;
+                    } else if (leftsquare.type.equals(mySheep)) {
+                        leftsquare.value = 0;
+                    } else {
+                        throw new java.lang.RuntimeException("could not determine type of leftsquare to assign a value");
+                    }
+
+                    //add to hashmap
+                    mapWithValues.put(getStringCoordinate(leftsquare, 0, 0), leftsquare);
+                    //add to expandQueue
+                    squareInitializeExpandQueue.add(leftsquare);
                 }
             }
 
             //check if outside map. Map is always 15x19 squares, see assignment. Or distance is too great because resource limits (time especially).
-            if(origin.yCoor > 14|| origin.distance > MAXDISTANCE){} else {
-                Square downsquare = new Square(map[yPos + 1][xPos], origin.xCoor, origin.yCoor + 1, origin.distance + 1, 0, 0);
-                if (downsquare.type.equals(Type.EMPTY)) {
-                    downsquare.value = 0;
-                } else if (downsquare.type.equals(Type.GRASS)) {
-                    downsquare.value = 1;
-                } else if (downsquare.type.equals(Type.RHUBARB)) {
-                    downsquare.value = 5;
-                } else if (downsquare.type.equals(Type.FENCE)) {
-                    downsquare.value = -999;
-                } else if (downsquare.type.equals(Type.WOLF1)) {
-                    downsquare.value = -999;
-                } else if (downsquare.type.equals(Type.WOLF2)) {
-                    downsquare.value = -999;
-                } else if (downsquare.type.equals(enemySheep)) {
-                    downsquare.value = -999;
-                } else if (downsquare.type.equals(mySheep)) {
-                    downsquare.value = 0;
+            if (origin.yCoor+1 > 14 || origin.distance > MAXDISTANCE) {
+            } else {
+                //checks if exists already in Hashmap
+                if (mapWithValues.containsKey(getStringCoordinate(origin, 1, 1))) {
                 } else {
-                    throw new java.lang.RuntimeException("could not determine type of downsquare to assign a value");
+                    Square downsquare = new Square(map[yPos + 1][xPos], origin.xCoor, origin.yCoor + 1, origin.distance + 1, 0, 0);
+                    if (downsquare.type.equals(Type.EMPTY)) {
+                        downsquare.value = 0;
+                    } else if (downsquare.type.equals(Type.GRASS)) {
+                        downsquare.value = 1;
+                    } else if (downsquare.type.equals(Type.RHUBARB)) {
+                        downsquare.value = 5;
+                    } else if (downsquare.type.equals(Type.FENCE)) {
+                        downsquare.value = -999;
+                    } else if (downsquare.type.equals(Type.WOLF1)) {
+                        downsquare.value = -999;
+                    } else if (downsquare.type.equals(Type.WOLF2)) {
+                        downsquare.value = -999;
+                    } else if (downsquare.type.equals(enemySheep)) {
+                        downsquare.value = -999;
+                    } else if (downsquare.type.equals(mySheep)) {
+                        downsquare.value = 0;
+                    } else {
+                        throw new java.lang.RuntimeException("could not determine type of downsquare to assign a value");
+                    }
+                    //add to hashmap
+                    mapWithValues.put(getStringCoordinate(downsquare, 0, 0), downsquare);
+                    //add to expandQueue
+                    squareInitializeExpandQueue.add(downsquare);
                 }
             }
-
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //do not add square since it is outside of the play board
         }
+
+        //add to the origin square that he has already been expanded
+
+
     }
 
+
     //at end when whole map is initialized, search if the enemy wolf appears. if yes, mark immediate neighbor squares as -999 value.
+
+
 
     //make data structure for Squares with values yCoor,xCoor,cost,value,totalValue.
     //Make datastructure like in greedy. Hashmap with string and Square type. The string is the Coordinate (yCoor_xCoor).
@@ -191,12 +238,14 @@ public class Sheep extends UzhShortNameCreature {
     private class Square {
         private Type type;
         private int yCoor, xCoor, distance, value, totalValue;
-        private boolean initExpanded = false;
 
         private Square(Type type, int xCoor, int yCoor, int distance, int value, int totalValue) {
             this.type = type;
             this.xCoor = xCoor;
             this.yCoor = yCoor;
         }
+    }
+    protected String getStringCoordinate(Square square, int yShift, int xShift) {
+        return Integer.toString(square.xCoor + yShift) + "_" + Integer.toString(square.xCoor + xShift);
     }
 }
